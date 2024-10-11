fa7cb420dcee98a50dd98f53ba1cbf7bf8d61522
姚琳 <yaolin0616@163.com>
2024-10-11 17:22:34 +0800

    添加PLL 3输出 65M

--- sys_top.v
+++ sys_top.v
@@ -1,53 +1,66 @@
 `timescale 1ns / 1ps
 module sys_top(
-    input                 sys_clk     	,  //系统时钟
-    input                 sys_rst_n   	,  //系统复位，低电平有效
-    // input                 key   		,  //按键key0    
-    //DA芯片接口
-    // output                da_clk      	,  //DA驱动时钟,最大支持125Mhz时钟
-    // output    [7:0]       da_data     	,  //输出给DA的数据
-    //AD芯片接口
-    input     [13:0]       ad_data    	,  //AD输入数据
-    //模拟输入电压超出量程标志(本次试验未用到)
-    input                 ad_porta_otr      	,  //0:在量程范围 1:超出量程
-    output                ada_porta_clk      	,  //AD驱动时钟,最大支持32Mhz时钟 
+    input                 sys_clk     	,  //绯荤粺鏃堕挓
+    input                 sys_rst_n   	,  //绯荤粺澶嶄綅锛屼綆鐢靛钩鏈夋晥
+    // input                 key   		,  //鎸夐敭key0    
+    //DA鑺墖鎺ュ彛
+    // output                da_clk      	,  //DA椹卞姩鏃堕挓,锟�?澶ф敮锟�?125Mhz鏃堕挓
+    // output    [7:0]       da_data     	,  //杈撳嚭缁橠A鐨勬暟锟�?
+    //AD鑺墖鎺ュ彛
+    input     [13:0]       ad_data    	,  //AD杈撳叆鏁版嵁
+    //妯℃嫙杈撳叆鐢靛帇瓒呭嚭閲忕▼鏍囧織(鏈璇曢獙鏈敤锟�?)
+    input                 ad_porta_otr      	,  //0:鍦ㄩ噺绋嬭寖锟�? 1:瓒呭嚭閲忕▼
+    output                ada_porta_clk      	,  //AD椹卞姩鏃堕挓,锟�?澶ф敮锟�?32Mhz鏃堕挓 
 
-        //模拟输入电压超出量程标志(本次试验未用到)
-    input                 ad_portb_otr      	,  //0:在量程范围 1:超出量程
-    output                ada_portb_clk      	,  //AD驱动时钟,最大支持32Mhz时钟 
+        //妯℃嫙杈撳叆鐢靛帇瓒呭嚭閲忕▼鏍囧織(鏈璇曢獙鏈敤锟�?)
+    input                 ad_portb_otr      	,  //0:鍦ㄩ噺绋嬭寖锟�? 1:瓒呭嚭閲忕▼
+    output                ada_portb_clk      	  //AD椹卞姩鏃堕挓,锟�?澶ф敮锟�?32Mhz鏃堕挓 
     
-    // output                lcd_de		,  //LCD 数据使能信号
-    // output                lcd_hs		,  //LCD 行同步信号
-    // output                lcd_vs		,  //LCD 场同步信号
-    // output                lcd_clk		,  //LCD 像素时钟
-    // inout     [23:0]   	  lcd_rgb		,  //LCD RGB888颜色数据
-    // output                lcd_rst		,  //LCD 复位,低电平有效 
-    // output                lcd_bl           //LCD 背光控制信号
+    // output                lcd_de		,  //LCD 鏁版嵁浣胯兘淇″彿
+    // output                lcd_hs		,  //LCD 琛屽悓姝ヤ俊锟�?
+    // output                lcd_vs		,  //LCD 鍦哄悓姝ヤ俊锟�?
+    // output                lcd_clk		,  //LCD 鍍忕礌鏃堕挓
+    // inout     [23:0]   	  lcd_rgb		,  //LCD RGB888棰滆壊鏁版嵁
+    // output                lcd_rst		,  //LCD 澶嶄綅,浣庣數骞虫湁锟�? 
+    // output                lcd_bl           //LCD 鑳屽厜鎺у埗淇″彿
     );
 
     //NET define    
-    wire        clk_100m;            //100m时钟
-    wire        clk_50m;             //50m时钟
-    wire        clk_25m;             //25m时钟 
-    wire        clk_25m_deg;         //相位偏移后的25m时钟 
-    wire        locked;              //pll输出稳定信号
-    wire        rst_n;               //复位信号，低电平有效
-    //待时钟锁定后产生结束复位信号
+    wire        clk_65m;            //100m鏃堕挓
+    wire        clkA_65m;             //50m鏃堕挓
+    wire        clkB_65m;             //25m鏃堕挓 
+    //wire        clkB_65m_deg;         //鐩镐綅鍋忕Щ鍚庣殑25m鏃堕挓 
+    wire        locked;              //pll杈撳嚭绋冲畾淇″彿
+    wire        rst_n;               //澶嶄綅淇″彿锛屼綆鐢靛钩鏈夋晥
+    //寰呮椂閽熼攣瀹氬悗浜х敓缁撴潫澶嶄綅淇″彿
     assign rst_n =  sys_rst_n && locked; 
-    //将pll产生的25m时钟赋给ad的驱动时钟
-    assign ad_clk =  clk_25m; 
+    //灏唒ll浜х敓锟�?25m鏃堕挓璧嬬粰ad鐨勯┍鍔ㄦ椂锟�?
+    assign ad_clk =  clkB_65m; 
 
-    //例化pll模块	
-    pll u_pll(
-    .clk_out1(clk_100m),    
-    .clk_out2(clk_50m),  
-    .clk_out3(clk_25m),     
-    .clk_out4(clk_25m_deg),     
-    // Status and control signals
-    .resetn(sys_rst_n), 
-    .locked(locked),      
-   // Clock in ports
-    .clk_in1(sys_clk)     
-    );   
+    //渚嬪寲pll妯″潡	
+      clk_wiz_0 u_pll
+  (
+  // Clock out ports  
+  .clk_out1(clk_65m),
+  .clk_out2(clkA_65m),
+  .clk_out3(clkB_65m),
+  // Status and control signals               
+  .resetn(sys_rst_n), 
+  .locked(locked),
+ // Clock in ports
+  .clk_in1(sys_clk)
+  );
+  
+//    pll u_pll(
+//    .clk_out1(clk_65m),    
+//    .clk_out2(clkA_65m),  
+//    .clk_out3(clkB_65m),     
+//    .clk_out4(clkB_65m_deg),     
+//    // Status and control signals
+//    .resetn(sys_rst_n), 
+//    .locked(locked),      
+//   // Clock in ports
+//    .clk_in1(sys_clk)     
+//    );   
 
 endmodule
