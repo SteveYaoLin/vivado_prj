2e3fcc670d2d712f45a8c0b77134790e27b3a412
姚琳 <yaolin0616@163.com>
2024-10-16 09:39:41 +0800

    新增FMC桥模块，top模块增加相关内容，tmp中管脚约束。已经实现成功。

---
+++ fsmc_bridge.v
@@ -0,0 +1,85 @@
+/* 
+ * --------------------
+ * Project Name			: gkds_sys 
+ * Module Name				: fsmc_driver 
+ * Description				: The codes of "fsmc_driver"
+ * --------------------
+ * Tool Versions			: Quartus II 13.1
+ * Target Device			: Cyclone IV E  EP4CE10F17C8
+ * --------------------
+ * Engineer					: weicguodong
+ * Revision					: V0.0
+ * Created Date			: 2019-11-28
+ * --------------------
+ * Engineer					:
+ * Revision					:
+ * Modified Date			:
+ * --------------------
+ * Additional Comments	: 
+ * 
+ * --------------------
+ */
+ /****************************************************************/
+ //timescale
+`timescale 1 ns / 1 ps
+
+/****************************************************************/
+module fsmc_bridge(
+	input sys_clk,
+	input rst_n,
+	
+	//fsmc总线相关信号
+
+	input fsmc_nadv,
+	input fsmc_wr,
+	input fsmc_rd,
+	input fsmc_cs,
+	inout [15:0]fsmc_db ,
+
+	//外部接口
+	// output  [0:0]  BUS_CLK,
+ 	output  [31:0] BUS_ADDR,
+ 	// output  [3:0]  BUS_BE,
+ 	output  [31:0] BUS_DATA_WR,
+ 	input [31:0] BUS_DATA_RD
+); 
+
+/****************************************************************/
+//wr和rd信号提取
+wire rdn = fsmc_cs | fsmc_rd;
+wire wrn = fsmc_cs | fsmc_wr;
+
+/****************************************************************/
+//接收地址
+reg [15:0]address_reg;
+always@(posedge fsmc_nadv or negedge rst_n)
+	begin
+		if(!rst_n)
+			begin
+				address_reg <= 24'd0;
+			end
+		else
+			begin
+				address_reg <= fsmc_db;
+			end
+	end
+
+//接收数据
+reg [15:0]ad_parameter;
+always@(posedge wrn or negedge rst_n)
+	begin
+		if(!rst_n)
+			begin
+				ad_parameter <= 16'd0;
+			end
+		else	
+			begin
+				ad_parameter <= fsmc_db;
+			end
+		
+	end
+assign BUS_ADDR = address_reg;
+assign BUS_DATA_WR = wrn ? fsmc_db : 16'dzzzz;
+assign fsmc_db = rdn ?  BUS_DATA_RD : 16'dzzzz;
+/****************************************************************/
+endmodule
--- sys_top.v
+++ sys_top.v
@@ -3,7 +3,16 @@ module sys_top(
     input                 sys_clk     	,  //时钟信号
     input                 sys_rst_n   	,  //复位信号
     // input                 key   		,  //按键信号
-
+    //STM32H7的FMC接口
+    inout     [15:0]       fmc_adda_data     ,  //FMC的ADDA数据
+    input     fmc_clk     ,  //FMC的ADDA时钟
+    input     fmc_nl        ,  //低电平有效的FMC总线忙闲信号
+    output    fmc_nwait     ,  //低电平有效的FMC总线等待信号
+    output    fmc_nwe       ,  //低电平有效的FMC总线写使能信号
+    output    fmc_ncs       ,  //低电平有效的FMC总线片选信号
+    output    fmc_noe       ,  //低电平有效的FMC总线读使能信号
+    output    fmc_int       ,  //MCU的中断信号
+    output    mcu_int       ,  //MCU的中断信号
     //AD转换模块的接口
     input     [13:0]       ad_porta_data    	,  //AD转换模块的数据
     input     [13:0]       ad_portb_data    	,  //AD转换模块的数据
@@ -25,6 +34,10 @@ module sys_top(
     wire        clkB_65m;             //25MHz时钟
     wire        locked;              //PLL锁定信号
     wire        rst_n;               //系统复位信号
+    wire  [31:0] BUS_ADDR;
+//   wire  [3:0]  BUS_BE；
+    wire  [31:0] BUS_DATA_WR;
+    wire  [31:0] BUS_DATA_RD;
 
     assign rst_n =  sys_rst_n && locked; 
     assign ad_shdna = 1'b1;
@@ -45,9 +58,30 @@ module sys_top(
     .clk_in1(sys_clk)
     );
   
-    //时钟信号缓冲
+    //instance bus bridge
+fsmc_bridge u_fsmc_bridge(
+	.sys_clk(sys_clk),
+	.rst_n(rst_n),
+	
+	//fsmc总线相关信号
+	.fsmc_nadv(fmc_nl),
+	.fsmc_wr(fmc_nwe),
+	.fsmc_rd(fmc_noe),
+	.fsmc_cs(fmc_ncs),
+	.fsmc_db(fmc_adda_data),
+
+	//外部接口
+	//.BUS_CLK(BUS_CLK),
+	.BUS_ADDR(BUS_ADDR),
+	//.BUS_BE(BUS_BE),
+	.BUS_DATA_WR(BUS_DATA_WR),
+	.BUS_DATA_RD(BUS_DATA_RD)
+);
+ //时钟信号缓冲
     BUFG bufa (.I(clkA_65m),.O(ad_porta_clk));
     BUFG bufb (.I(clkB_65m),.O(ad_portb_clk));
 
+// compile
+assign  BUS_DATA_RD = ad_porta_data;
 
 endmodule
